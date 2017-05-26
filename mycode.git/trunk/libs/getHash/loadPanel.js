53fb9a1def7bcdaa081fa0a06475e6e4b6bfb435
lovedwhom@live.com <shuaige8023>
2017-05-27 00:38:42 +0800

    切换地址时卸载css

--- mycode.git/trunk/libs/getHash/loadPanel.js
+++ mycode.git/trunk/libs/getHash/loadPanel.js
@@ -1,16 +1,30 @@
 /**
  * Created by Administrator on 2017/5/25 0025.
  */
+//记录页面名称
+var curPageName = '';
 
 //*************页面路由配置*******************/
 function loadPanel() {
+    console.log('1111');
     var page = getHash('page');
+
+    if(isNotNull(curPageName))
+    {
+        //动态卸载页面对应css
+        removejscssfile(curPageName+".css", "css");
+    }
     var curPageName = page;
     console.log(page);
     var mainbody =$('#container');
     console.log(mainbody);
     switch (page){
         case 'home':
+        {
+            modelPath = './pages/' + curPageName + '.js';
+            break;
+        }
+        case 'test1':
         {
             modelPath = './pages/' + curPageName + '.js';
             break;
