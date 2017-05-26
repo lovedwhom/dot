73ede67335b65fde4a07bdd8aea00d2a03209735
lovedwhom@live.com <shuaige8023>
2017-05-27 00:41:44 +0800

    增加dot简单应用

--- mycode.git/trunk/pages/home.js
+++ mycode.git/trunk/pages/home.js
@@ -7,9 +7,21 @@ define(function(require, exports, module){
     // alert('1');
     exports.show =function (opt) {
         var template =require('./home.html');
-        console.log(template);
         template = doT.template(template);
         opt.body.html(template(''));
         // $(document.body).append('<div>seaJS的second模块被正常加载。</div>')
+
+        $('#container').on('click','p',function () {
+            var _that = this;
+            // console.log(111)
+            // console.log(this.id);
+            // try{
+            //     window.location.herf = 'index.html#page=' + _that.id;
+            // }catch(e){
+            //     console.log(e);
+            // }
+
+            window.location.href='index.html#page=test1'
+        })
     }
 });
